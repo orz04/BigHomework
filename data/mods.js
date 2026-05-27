b6b62eeaf62a741f8b28f53e6d12469dba7ea535
MYJ05 <744132023@qq.com>
2026-05-27 15:05:37 +0800

    补充mod信息，新添mod信息

--- data/mods.js
+++ data/mods.js
@@ -3,7 +3,6 @@ module.exports = {
     {
       title: '毒素弹幕',
       image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/CorruptedVorimg/%E6%AF%92%E7%B4%A0%E5%BC%B9%E5%B9%95.png?sign=354123eb8935b9b48e42849670bd2daa&t=1779863049',
-      type: '霰弹枪',
       polarity: 'Madurai',
       polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
       rarity: '稀有',
@@ -39,7 +38,6 @@ module.exports = {
     {
       title: '瘟疫手枪',
       image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/CorruptedVorimg/%E7%98%9F%E7%96%AB%E6%89%8B%E6%9E%AA.png?sign=a8113d0ac5c94df0d6fd0934d2338ce8&t=1779864020',
-      type: '手枪',
       polarity: 'Madurai',
       polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
       rarity: '稀有',
@@ -75,7 +73,6 @@ module.exports = {
     {
       title: '剧毒灾害',
       image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/CorruptedVorimg/%E5%89%A7%E6%AF%92%E7%81%BE%E5%AE%B3.png?sign=e39eb7eab6e204a371d8b4eca4a2bde7&t=1779864032',
-      type: '近战',
       polarity: 'Madurai',
       polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
       rarity: '稀有',
@@ -83,7 +80,7 @@ module.exports = {
       tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
       drain: '4',
       maxRank: '3',
-      category: '手枪',
+      category: '近战',
       labels: {
         basicInfo: '基本信息',
         polarity: '极性',
@@ -111,14 +108,13 @@ module.exports = {
     {
       title: '致命火力',
       image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/CorruptedVorimg/%E8%87%B4%E5%91%BD%E7%81%AB%E5%8A%9B.png?sign=08272319cc0fac935fefd713eb809fa4&t=1779864042',
-      type: '步枪',
       polarity: 'Madurai',
-      polarityIcon: '/images/modimg/Polarityimg/Aura.png',
-      rarity: '传说',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
+      rarity: '稀有',
       tradeTax: '8,000',
-      tradeTaxIcon: '/images/modimg/Polarityimg/Aura.png',
-      drain: '6',
-      maxRank: '10',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '4',
+      maxRank: '3',
       category: '步枪',
       labels: {
         basicInfo: '基本信息',
@@ -132,9 +128,9 @@ module.exports = {
         detailInfo: '详细信息'
       },
       modSource: {
-        type: '任务',
-        location: '火卫二',
-        rate: '4.17%'
+        type: '敌人',
+        location: '堕落的沃尔',
+        rate: '25%'
       },
       upgradeHeader: ['等级', '毒素伤害', '触发几率', '消耗'],
       upgradeData: [
