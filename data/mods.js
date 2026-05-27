df7ea070832872b45cc708ffa48570e7ffea8e29
MYJ05 <744132023@qq.com>
2026-05-27 15:07:41 +0800

    新增空白模板

--- data/mods.js
+++ data/mods.js
@@ -139,6 +139,454 @@ module.exports = {
         { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
         { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
       ]
+    },
+    {
+      title: '重口径',
+      image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Orokinimg/%E9%87%8D%E5%8F%A3%E5%BE%84.png?sign=e68ff3697b80d4aaf0023bf4114cf7c3&t=1779864817',
+      polarity: 'Madurai',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '6',
+      maxRank: '10',
+      category: '步枪',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '任务',
+        location: '火卫二宝库',
+        rate: '4.17%'
+      },
+      upgradeHeader: ['等级', '伤害', '精准度', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '-5%', drain: '6' },
+        { level: '1', attr1: '+30%', attr2: '-10%', drain: '7' },
+        { level: '2', attr1: '+45%', attr2: '-15%', drain: '8' },
+        { level: '3', attr1: '+60%', attr2: '-20%', drain: '9' },
+        { level: '4', attr1: '+75%', attr2: '-25%', drain: '10' },
+        { level: '5', attr1: '+90%', attr2: '-30%', drain: '11' },
+        { level: '6', attr1: '+105%', attr2: '-35%', drain: '12' },
+        { level: '7', attr1: '+120%', attr2: '-40%', drain: '13' },
+        { level: '8', attr1: '+135%', attr2: '-45%', drain: '14' },
+        { level: '9', attr1: '+150%', attr2: '-50%', drain: '15' },
+        { level: '10', attr1: '+165%', attr2: '-55%', drain: '16' }
+      ]
+    },
+    {
+      title: '瞬时坚毅',
+      image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Orokinimg/%E7%9E%AC%E6%97%B6%E5%9D%9A%E6%AF%85.png?sign=a08400f239432d3f4364a417eaaffd89&t=1779865060',
+      polarity: 'Madurai',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '6',
+      maxRank: '10',
+      category: '战甲',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '任务',
+        location: '火卫二',
+        rate: '4.17%'
+      },
+      upgradeHeader: ['等级', '技能强度', '持续时间', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+5%', attr2: '-2.5%', drain: '6' },
+        { level: '1', attr1: '+10%', attr2: '-5%', drain: '7' },
+        { level: '2', attr1: '+15%', attr2: '-7.5%', drain: '8' },
+        { level: '3', attr1: '+20%', attr2: '-10%', drain: '9' },
+        { level: '4', attr1: '+25%', attr2: '-12.5%', drain: '10' },
+        { level: '5', attr1: '+30%', attr2: '-15%', drain: '11' },
+        { level: '6', attr1: '+35%', attr2: '-17.5%', drain: '12' },
+        { level: '7', attr1: '+40%', attr2: '-20%', drain: '13' },
+        { level: '8', attr1: '+45%', attr2: '-22.5%', drain: '14' },
+        { level: '9', attr1: '+50%', attr2: '-25%', drain: '15' },
+        { level: '10', attr1: '+55%', attr2: '-27.5%', drain: '16' }
+      ]
+    },
+    {
+      title: '盲怒',
+      image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Orokinimg/%E7%9B%B2%E6%80%92.png?sign=2ede9b4bfe42d01123b498535f3a71e7&t=1779865205',
+      polarity: 'Madurai',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=59171931b5114a60ac57c2a6cd867833&t=1779862985',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '6',
+      maxRank: '10',
+      category: '战甲',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '任务',
+        location: '火卫二',
+        rate: '4.17%'
+      },
+      upgradeHeader: ['等级', '技能效率', '技能强度', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '-5%', attr2: '+9%', drain: '6' },
+        { level: '1', attr1: '-10%', attr2: '+18%', drain: '7' },
+        { level: '2', attr1: '-15%', attr2: '+27%', drain: '8' },
+        { level: '3', attr1: '-20%', attr2: '+36%', drain: '9' },
+        { level: '4', attr1: '-25%', attr2: '+45%', drain: '10' },
+        { level: '5', attr1: '-30%', attr2: '+54%', drain: '11' },
+        { level: '6', attr1: '-35%', attr2: '+63%', drain: '12' },
+        { level: '7', attr1: '-40%', attr2: '+72%', drain: '13' },
+        { level: '8', attr1: '-45%', attr2: '+81%', drain: '14' },
+        { level: '9', attr1: '-50%', attr2: '+90%', drain: '15' },
+        { level: '10', attr1: '-55%', attr2: '+99%', drain: '16' }
+      ]
+    },
+    {
+      title: '心志偏狭',
+      image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Orokinimg/%E5%BF%83%E5%BF%97%E5%81%8F%E7%8B%AD.png?sign=397305e66ea41e5f29481712d2888805&t=1779865370',
+      polarity: 'Vazarin',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Vazarin.png?sign=5fbc13ae2921c289438c40261098dceb&t=1779865419',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '6',
+      maxRank: '10',
+      category: '战甲',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '任务',
+        location: '火卫二',
+        rate: '4.17%'
+      },
+      upgradeHeader: ['等级', '技能范围', '持续时间', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '-6%', attr2: '+9%', drain: '6' },
+        { level: '1', attr1: '-12%', attr2: '+18%', drain: '7' },
+        { level: '2', attr1: '-18%', attr2: '+27%', drain: '8' },
+        { level: '3', attr1: '-24%', attr2: '+36%', drain: '9' },
+        { level: '4', attr1: '-30%', attr2: '+45%', drain: '10' },
+        { level: '5', attr1: '-36%', attr2: '+54%', drain: '11' },
+        { level: '6', attr1: '-42%', attr2: '+63%', drain: '12' },
+        { level: '7', attr1: '-48%', attr2: '+72%', drain: '13' },
+        { level: '8', attr1: '-54%', attr2: '+81%', drain: '14' },
+        { level: '9', attr1: '-60%', attr2: '+90%', drain: '15' },
+        { level: '10', attr1: '-66%', attr2: '+99%', drain: '16' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
+    },
+    {
+      title: '',
+      image: '',
+      polarity: '',
+      polarityIcon: '',
+      rarity: '稀有',
+      tradeTax: '8,000',
+      tradeTaxIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=b8c282aab22fffa1f3b6dac5e190a4f9&t=1779863207',
+      drain: '',
+      maxRank: '',
+      category: '',
+      labels: {
+        basicInfo: '基本信息',
+        polarity: '极性',
+        rarity: '稀有度',
+        tradeTax: '交易税',
+        drain: '基础消耗量',
+        maxRank: '最高等级',
+        category: '适用于',
+        modSource: '获取来源',
+        detailInfo: '详细信息'
+      },
+      modSource: {
+        type: '',
+        location: '',
+        rate: '%'
+      },
+      upgradeHeader: ['等级', '', '', '消耗'],
+      upgradeData: [
+        { level: '0', attr1: '+15%', attr2: '+15%', drain: '4' },
+        { level: '1', attr1: '+30%', attr2: '+30%', drain: '5' },
+        { level: '2', attr1: '+45%', attr2: '+45%', drain: '6' },
+        { level: '3', attr1: '+60%', attr2: '+60%', drain: '7' }
+      ]
     }
   ]
 }
