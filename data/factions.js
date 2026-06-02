fd6fda9a83925fda5d123dcf695dd09dedcfd7c6
orz04 <3268103890@qq.com>
2026-06-02 12:06:20 +0800

    完善派系集团信息路径

--- data/factions.js
+++ data/factions.js
@@ -4,27 +4,171 @@ module.exports = [
     groups: [
       {
         name: '钢铁防线',
-        image: '/images/groupimg/SteelMeridian.png'
+        page: '/pages/faction-detail/faction-detail?name=钢铁防线',
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/SteelMeridian.png?sign=7013c6ba605fbe3a0b775c66021126da&t=1780369695',
+        relations: [
+          {
+            name: '血色面纱',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/RedVeil.png?sign=176d2ece82b2acab694dda1f822939c1&t=1780369766',
+            type: '盟友',
+            standing: '+50% 声望',
+            standingClass: 'ally'
+          },
+          {
+            name: '新世间',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/NewLoka.png?sign=21d8c7ba5cb1ad7aa0a2d637119b41c4&t=1780369816',
+            type: '对立',
+            standing: '-50% 声望',
+            standingClass: 'opposed'
+          },
+          {
+            name: '佩兰数列',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ThePerrinSequence.png?sign=2a4866b283502256fe90f2d4817f0d62&t=1780369891',
+            type: '敌人',
+            standing: '-100% 声望',
+            standingClass: 'enemy'
+          }
+        ]
       },
       {
         name: '均衡仲裁者',
-        image: '/images/groupimg/ArbitersofHexis.png'
+        page: '/pages/faction-detail/faction-detail?name=均衡仲裁者',
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ArbitersofHexis.png?sign=3a3308ba5993a9f45d0b8c8741dc7028&t=1780369945',
+        relations: [
+          {
+            name: '中枢苏达',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/CephalonSuda.png?sign=73b8865d6c209545949d6b9490f29609&t=1780370004',
+            type: '盟友',
+            standing: '+50% 声望',
+            standingClass: 'ally'
+          },
+          {
+            name: '佩兰数列',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ThePerrinSequence.png?sign=2a4866b283502256fe90f2d4817f0d62&t=1780369891',
+            type: '对立',
+            standing: '-50% 声望',
+            standingClass: 'opposed'
+          },
+          {
+            name: '血色面纱',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/RedVeil.png?sign=176d2ece82b2acab694dda1f822939c1&t=1780369766',
+            type: '敌人',
+            standing: '-100% 声望',
+            standingClass: 'enemy'
+          }
+        ]
       },
       {
         name: '中枢苏达',
-        image: '/images/groupimg/CephalonSuda.png'
+        page: '/pages/faction-detail/faction-detail?name=中枢苏达',
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/CephalonSuda.png?sign=73b8865d6c209545949d6b9490f29609&t=1780370004',
+        relations: [
+          {
+            name: '均衡仲裁者',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ArbitersofHexis.png?sign=3a3308ba5993a9f45d0b8c8741dc7028&t=1780369945',
+            type: '盟友',
+            standing: '+50% 声望',
+            standingClass: 'ally'
+          },
+          {
+            name: '血色面纱',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/RedVeil.png?sign=176d2ece82b2acab694dda1f822939c1&t=1780369766',
+            type: '对立',
+            standing: '-50% 声望',
+            standingClass: 'opposed'
+          },
+          {
+            name: '新世间',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/NewLoka.png?sign=21d8c7ba5cb1ad7aa0a2d637119b41c4&t=1780369816',
+            type: '敌人',
+            standing: '-100% 声望',
+            standingClass: 'enemy'
+          }
+        ]
       },
       {
         name: '佩兰数列',
-        image: '/images/groupimg/ThePerrinSequence.png'
+        page: '/pages/faction-detail/faction-detail?name=佩兰数列',
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ThePerrinSequence.png?sign=2a4866b283502256fe90f2d4817f0d62&t=1780369891',
+        relations: [
+          {
+            name: '新世间',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/NewLoka.png?sign=21d8c7ba5cb1ad7aa0a2d637119b41c4&t=1780369816',
+            type: '盟友',
+            standing: '+50% 声望',
+            standingClass: 'ally'
+          },
+          {
+            name: '钢铁防线',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/SteelMeridian.png?sign=7013c6ba605fbe3a0b775c66021126da&t=1780369695',
+            type: '对立',
+            standing: '-50% 声望',
+            standingClass: 'opposed'
+          },
+          {
+            name: '均衡仲裁者',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ArbitersofHexis.png?sign=3a3308ba5993a9f45d0b8c8741dc7028&t=1780369945',
+            type: '敌人',
+            standing: '-100% 声望',
+            standingClass: 'enemy'
+          }
+        ]
       },
       {
         name: '血色面纱',
-        image: '/images/groupimg/RedVeil.png'
+        page: '/pages/faction-detail/faction-detail?name=血色面纱',
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/RedVeil.png?sign=176d2ece82b2acab694dda1f822939c1&t=1780369766',
+        relations: [
+          {
+            name: '钢铁防线',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/SteelMeridian.png?sign=7013c6ba605fbe3a0b775c66021126da&t=1780369695',
+            type: '盟友',
+            standing: '+50% 声望',
+            standingClass: 'ally'
+          },
+          {
+            name: '中枢苏达',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/CephalonSuda.png?sign=73b8865d6c209545949d6b9490f29609&t=1780370004',
+            type: '对立',
+            standing: '-50% 声望',
+            standingClass: 'opposed'
+          },
+          {
+            name: '均衡仲裁者',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ArbitersofHexis.png?sign=3a3308ba5993a9f45d0b8c8741dc7028&t=1780369945',
+            type: '敌人',
+            standing: '-100% 声望',
+            standingClass: 'enemy'
+          }
+        ]
       },
       {
         name: '新世间',
-        image: '/images/groupimg/NewLoka.png'
+        page: '/pages/faction-detail/faction-detail?name=新世间',
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/NewLoka.png?sign=21d8c7ba5cb1ad7aa0a2d637119b41c4&t=1780369816',
+        relations: [
+          {
+            name: '佩兰数列',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/ThePerrinSequence.png?sign=2a4866b283502256fe90f2d4817f0d62&t=1780369891',
+            type: '盟友',
+            standing: '+50% 声望',
+            standingClass: 'ally'
+          },
+          {
+            name: '钢铁防线',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/SteelMeridian.png?sign=7013c6ba605fbe3a0b775c66021126da&t=1780369695',
+            type: '对立',
+            standing: '-50% 声望',
+            standingClass: 'opposed'
+          },
+          {
+            name: '中枢苏达',
+            image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/CephalonSuda.png?sign=73b8865d6c209545949d6b9490f29609&t=1780370004',
+            type: '敌人',
+            standing: '-100% 声望',
+            standingClass: 'enemy'
+          }
+        ]
       }
     ]
   },
@@ -33,55 +177,55 @@ module.exports = [
     groups: [
       {
         name: '武形秘仪',
-        image: '/images/groupimg/Conclave.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Conclave.png?sign=3873a191774eb640963f764f7b6e6e6c&t=1780370077'
       },
       {
         name: '中枢SIMARIS',
-        image: '/images/groupimg/CephalonSimaris.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/CephalonSimaris.png?sign=b80f94403781662bea486b252f44d60f&t=1780370091'
       },
       {
         name: 'OSTRON',
-        image: '/images/groupimg/Ostron.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Ostron.png?sign=d0f2c5f7185479d2e6fffc81c6d09075&t=1780370103'
       },
       {
         name: '夜羽',
-        image: '/images/groupimg/TheQuills.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/TheQuills.png?sign=b22de12b5071c1b99ad7558b190bbb9d&t=1780370197'
       },
       {
         name: '索拉里斯联盟',
-        image: '/images/groupimg/SolarisAlliance.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/SolarisAlliance.png?sign=bfd72cecaca6099e08f355fcdd01fcea&t=1780370209'
       },
       {
         name: '通风小子',
-        image: '/images/groupimg/VentilationKid.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/VentilationKid.png?sign=652f30c9a0cc9cae67fcbc8aa7c0293d&t=1780370220'
       },
       {
         name: '索拉里斯之声（集团）',
-        image: '/images/groupimg/VoxSolaris.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/VoxSolaris.png?sign=891822dd402e0c30d97b1e367c902c4d&t=1780370231'
       },
       {
         name: '英择谛',
-        image: '/images/groupimg/Entrati.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Entrati.png?sign=3760e2d0eea5e08e261b0f00c8919e60&t=1780370243'
       },
       {
         name: '殁世械灵',
-        image: '/images/groupimg/Necraloid.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Necraloid.png?sign=1c2b9eb248a395cbf30308b0ace40318&t=1780370256'
       },
       {
         name: '坚守者',
-        image: '/images/groupimg/TheHoldfasts.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/TheHoldfasts.png?sign=df18586fcf480f6e742ec937b882b673&t=1780370270'
       },
       {
         name: '卡尔驻军',
-        image: '/images/groupimg/KahlsGarrison.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/KahlsGarrison.png?sign=f708bfe24b76cde7aa9cb342dc118afe&t=1780370281'
       },
       {
         name: '科维兽',
-        image: '/images/groupimg/Cavia.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Cavia.png?sign=35457529f01f61f59b7d36d1486307b4&t=1780370296'
       },
       {
         name: '六人组（集团）',
-        image: '/images/groupimg/Syndicate.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Syndicate.png?sign=0e79e59c4fb26c66fe45b894a58efb65&t=1780370306'
       }
     ]
   },
@@ -90,11 +234,11 @@ module.exports = [
     groups: [
       {
         name: '行动补给',
-        image: '/images/groupimg/Operationalsupplies.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Operationalsupplies.png?sign=11a194c4a11cf60b220cdbad0cc78e7d&t=1780370319'
       },
       {
         name: '午夜电波',
-        image: '/images/groupimg/Nightwave.png'
+        image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/groupimg/Nightwave.png?sign=fe9dd2b0f9bd19a2424136340f7911bb&t=1780370331'
       }
     ]
   }
