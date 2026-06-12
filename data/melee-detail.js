66f5802597dfa235bf43216c04f631154d644f2e
MYJ05 <744132023@qq.com>
2026-06-12 19:10:01 +0800

    空刃信息

--- data/melee-detail.js
+++ data/melee-detail.js
@@ -744,6 +744,96 @@ module.exports = {
         criticalChance: '10.0%',
         criticalDamage: '1.8x'
       }
+    },
+    {
+      title: '空刃',
+      image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/weaponimg/meleeimg/Skana.png?sign=e9c1cb7f43288ce6dc21f9a88bf646e3&t=1781254483',
+      labels: {
+        forging: '铸造需求',
+        basicInfo: '基础信息',
+        attackInfo: '普通攻击',
+        groundAttackInfo: '震地攻击',
+        heavyGroundAttackInfo: '重型震地攻击',
+        type: '武器类型',
+        slot: '武器槽位',
+        mastery: '精通段位要求',
+        range: '攻击范围',
+        normalRadius: '法向延展半径',
+        attackSpeed: '攻击速度',
+        blockAngle: '格挡角度',
+        comboDuration: '连击持续时间',
+        damagePenetration: '伤害穿透系数',
+        polarity: '极性',
+        riftPolarity: '裂罅倾向',
+        totalDamage: '总伤害',
+        noise: '噪音等级',
+        statusChance: '触发几率',
+        criticalChance: '暴击几率',
+        criticalDamage: '暴击伤害',
+        damageType: '伤害判定类型',
+        damageRange: '伤害范围',
+        damageDecay: '伤害衰减',
+        forcedStatus: '强制触发'
+      },
+      forging: {
+        materials: [
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=c2794ea8bbdccdccf862eaacb90d57ba&t=1779161854', amount: '15,000' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Salvage.png?sign=d726348c9e6a618a85acb1d1faf2a4e1&t=1781262569', amount: '750' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Plastids.png?sign=29b14b98a8d3ab83c370377b28755517&t=1781075770', amount: '500' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/AlloyPlate.png?sign=7fac282f4f4dc51eea1df978ec74ca92&t=1781262559', amount: '1,100' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Morphics.png?sign=ae28c5213a185646c13ed2599db0ad10&t=1781075735', amount: '3' }
+        ],
+        time: '12小时',
+        speed: '35',
+        prices: [
+          { type: '商店', icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/MarketIcon.png?sign=0a23c025a7c0550b41bebaea9945f50c&t=1779161909', currencyIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Platinum.png?sign=5cd80582566639d1f3e7404674d1d499&t=1779161920', amount: '150' },
+          { type: '蓝图', icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Blueprint2.svg.png?sign=928ea774ea82886cfacd530b63ddafb9&t=1779161893', currencyIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=c2794ea8bbdccdccf862eaacb90d57ba&t=1779161854', amount: '15,000' }
+        ]
+      },
+      type: '剑',
+      slot: '近战',
+      mastery: '无',
+      range: '2.5米',
+      normalRadius: '0.25米',
+      attackSpeed: '0.833倍基准攻速',
+      blockAngle: '55°',
+      comboDuration: '5.0秒',
+      damagePenetration: '60%',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Madurai.png?sign=44e7ab77b8d407b67db571d5b82bb262&t=1781258429',
+      polarity: 'Madurai',
+      riftPolarity: '1.3',
+      attackData: {
+        totalDamage: '120(70.0%)',
+        attackSpeed: '0.833倍',
+        noise: '无声',
+        statusChance: '16.0%',
+        criticalChance: '5.0%',
+        criticalDamage: '1.5x'
+      },
+      groundAttackData: {
+        totalDamage: '240(100.0%)',
+        attackSpeed: '0.833倍',
+        damageType: '范围伤害',
+        noise: '无声',
+        damageRange: '7米',
+        damageDecay: '0米内100%伤害<br/>7米外50.0%伤害',
+        statusChance: '10.0%',
+        forcedStatus: '冲击',
+        criticalChance: '5.0%',
+        criticalDamage: '1.5x'
+      },
+      heavyGroundAttackData: {
+        totalDamage: '360(100.0%)',
+        attackSpeed: '0.833倍',
+        damageType: '范围伤害',
+        noise: '无声',
+        damageRange: '8米',
+        damageDecay: '0米内100%伤害<br/>8米外70.0%伤害',
+        statusChance: '10.0%',
+        forcedStatus: '浮空',
+        criticalChance: '5.0%',
+        criticalDamage: '1.5x'
+      }
     }
   ]
 }
