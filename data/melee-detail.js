2ba8a21f16a257cf0531d538e7444e4048c964d5
MYJ05 <744132023@qq.com>
2026-06-12 17:47:37 +0800

    关刀信息完整

--- data/melee-detail.js
+++ data/melee-detail.js
@@ -449,6 +449,96 @@ module.exports = {
         criticalChance: '10.0%',
         criticalDamage: '2x'
       }
+    },
+    {
+      title: '关刀',
+      image: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/weaponimg/meleeimg/Guandao.png?sign=f7cdb5d1fa75b1ff80256e3e61631991&t=1781254349',
+      labels: {
+        forging: '铸造需求',
+        basicInfo: '基础信息',
+        attackInfo: '普通攻击',
+        groundAttackInfo: '震地攻击',
+        heavyGroundAttackInfo: '重型震地攻击',
+        type: '武器类型',
+        slot: '武器槽位',
+        mastery: '精通段位要求',
+        range: '攻击范围',
+        normalRadius: '法向延展半径',
+        attackSpeed: '攻击速度',
+        blockAngle: '格挡角度',
+        comboDuration: '连击持续时间',
+        damagePenetration: '伤害穿透系数',
+        polarity: '极性',
+        riftPolarity: '裂罅倾向',
+        totalDamage: '总伤害',
+        noise: '噪音等级',
+        statusChance: '触发几率',
+        criticalChance: '暴击几率',
+        criticalDamage: '暴击伤害',
+        damageType: '伤害判定类型',
+        damageRange: '伤害范围',
+        damageDecay: '伤害衰减',
+        forcedStatus: '强制触发'
+      },
+      forging: {
+        materials: [
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=c2794ea8bbdccdccf862eaacb90d57ba&t=1779161854', amount: '15,000' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Gallium.png?sign=25f2b1434c2c63b287e7598a76c90284&t=1781257501', amount: '3' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Oxium.png?sign=60e64bb0117a665974da8b86f6cfceb3&t=1781257514', amount: '65' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/PolymerBundle.png?sign=c34d7ce5c07a4e738e4389656b0eeb9c&t=1781257537', amount: '750' },
+          { icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Ferrite.png?sign=0576124b453d0c7ddcf73bd656d41f6a&t=1781257547', amount: '2,500' }
+        ],
+        time: '12小时',
+        speed: '35',
+        prices: [
+          { type: '商店', icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/MarketIcon.png?sign=0a23c025a7c0550b41bebaea9945f50c&t=1779161909', currencyIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Platinum.png?sign=5cd80582566639d1f3e7404674d1d499&t=1779161920', amount: '180' },
+          { type: '蓝图', icon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Blueprint2.svg.png?sign=928ea774ea82886cfacd530b63ddafb9&t=1779161893', currencyIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/resourceimg/Credits.png?sign=c2794ea8bbdccdccf862eaacb90d57ba&t=1779161854', amount: '15,000' }
+        ]
+      },
+      type: '长柄武器',
+      slot: '近战',
+      mastery: '4',
+      range: '3.0米',
+      normalRadius: '0.15米',
+      attackSpeed: '0.833倍基准攻速',
+      blockAngle: '55°',
+      comboDuration: '5.0秒',
+      damagePenetration: '60%',
+      polarityIcon: 'https://636c-cloud1-d5gj26hlf342afe88-1433985605.tcb.qcloud.la/images/modimg/Polarityimg/Zenurik.png?sign=269b90555495a8edb7396f82a0dd7c24&t=1781256980',
+      polarity: 'Zenurik',
+      riftPolarity: '1.05',
+      attackData: {
+        totalDamage: '202.0(70.0%)',
+        attackSpeed: '0.833倍',
+        noise: '无声',
+        statusChance: '4.0%',
+        criticalChance: '28.0%',
+        criticalDamage: '2.2x'
+      },
+      groundAttackData: {
+        totalDamage: '404(100.0%)',
+        attackSpeed: '0.833倍',
+        damageType: '范围伤害',
+        noise: '无声',
+        damageRange: '7米',
+        damageDecay: '0米内100%伤害<br/>7米外50.0%伤害',
+        statusChance: '10.0%',
+        forcedStatus: '冲击',
+        criticalChance: '28.0%',
+        criticalDamage: '2.2x'
+      },
+      heavyGroundAttackData: {
+        totalDamage: '606(100.0%)',
+        attackSpeed: '0.833倍',
+        damageType: '范围伤害',
+        noise: '无声',
+        damageRange: '8米',
+        damageDecay: '0米内100%伤害<br/>8米外70.0%伤害',
+        statusChance: '10.0%',
+        forcedStatus: '浮空',
+        criticalChance: '28.0%',
+        criticalDamage: '2.2x'
+      }
     }
   ]
 }
